d3f3b528a35c6295cc1f8985f978f539ed9c1ed2
vpandaxjl <jeromewell@163.com>
2016-05-16 10:22:40 +0800

    修改数据库为Mysql

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 SECRET_KEY = 'this is key xxx'
 
-SQLALCHEMY_DATABASE_URI='mysql://root@127.0.0.1/blog'
+SQLALCHEMY_DATABASE_URI='mysql://root@127.0.0.1/blog' ##mysql
 #SQLALCHEMY_DATABASE_URI='sqlite:///datadb.db'
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS  = True
